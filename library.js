7e44c4fc3dcbf78b68c35c94377c22986656cf41
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2021-06-30 14:57:08 -0300

    html e css feitos, terminar a logica do javascript

--- library.js
+++ library.js
@@ -1,47 +1,42 @@
 let myLibrary = [];
 let uniqueID = 0;
+const form = document.querySelector("form");
 
-function Book(author, title, pages, totalPages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.totalPages = totalPages;
-  this.read = read;
-  this.id = uniqueID;
-}
-
-function validateInput(pages, totalPages) {
-  if (totalPages < pages) {
-    return false
+class Book {
+  constructor(author, title, pagesRead, totalPages, read) {
+    this.author = author;
+    this.title = title;
+    this.pagesRead = pagesRead;
+    this.totalPages = totalPages;
+    this.read = read;
+    this.id = ++uniqueID;
   }
-return true;
+  showBooks () {
+    myLibrary.forEach((book) => console.log(book))
+  }
+  removeBooks() {
+  }
 }
 
 function addBookToLibrary() {
-  let title = document.forms[0].elements["title"].value;
-  let author = document.forms[0].elements["author"].value;
-  let pages = document.forms[0].elements["page-read"].value;
-  let totalPages = document.forms[0].elements["pages"].value;
-  let read = document.forms[0].elements["read"].checked;
-  let id = uniqueID++
-  if(!validateInput(pages, totalPages)){
-    return alert('Total of pages must be greater than pages read');
+  if(verifyForm() === false) {
+    return false;
   }
   
-  let book = new Book(author, title, pages, totalPages, read);
-  myLibrary.push(book);
+}
 
-  createBookDiv()
+function addBookToHTML(){
 
 }
 
-function createBookDiv() {
-  let library = document.querySelector(".library")
-  let bookDiv = document.createElement('div')
-  /*
-  * Remove the H1 when library has books 
-  * */
-  if (library)
-  library.appendChild(bookDiv);
+function verifyForm() {
+  if(+form.pagesRead.value > +form.totalPages.value) {
+    console.log(pagesRead > totalPages);
+    return false;
+  } 
 }
 
+// Prevent form from submitting 
+form.onsubmit = function(e){
+  e.preventDefault();
+  addBookToLibrary();}
